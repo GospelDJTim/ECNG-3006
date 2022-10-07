68870aff86eef61d8861ac9e74d4383eae14e77b
GospelDJTim <timothy.harripaul@my.uwi.edu>
2022-10-07 00:16:08 +0000

    Testing 1

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -15,7 +15,7 @@
 
 void app_main()
 {
-    printf("Hello world!\n");
+    printf("Timothy Harripaul 814001359\n");
 
     /* Print chip information */
     esp_chip_info_t chip_info;
@@ -28,7 +28,7 @@ void app_main()
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    for (int i = 10; i >= 0; i--) {
+    for (int i = 59 % 17) {
         printf("Restarting in %d seconds...\n", i);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
